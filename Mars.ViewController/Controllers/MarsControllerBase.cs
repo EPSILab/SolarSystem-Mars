7c3841f4c4f0921c16db10fc41a6dacc79df7312
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-03-30 23:19:15 +0200

    Edit Controller Base constructor

--- Mars.ViewController/Controllers/MarsControllerBase.cs
+++ Mars.ViewController/Controllers/MarsControllerBase.cs
@@ -1,4 +1,5 @@
 ﻿using Ninject;
+using SolarSystem.Mars.Model.Model.Abstract;
 using SolarSystem.Mars.ViewController.Infrastructure.Abstract;
 using System.Web.Mvc;
 
@@ -6,7 +7,39 @@ namespace SolarSystem.Mars.ViewController.Controllers
 {
     public abstract class MarsControllerBase : Controller
     {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        protected MarsControllerBase()
+        {
+        }
+
+        /// <summary>
+        /// Constructor. Resolve parameters with NInject
+        /// </summary>
+        protected MarsControllerBase(IConstants constants)
+        {
+            _constants = constants;
+        }
+
+        #endregion
+
+        #region Attributes
+
+        /// <summary>
+        /// Constants values
+        /// </summary>
+        protected readonly IConstants _constants;
+
+        #endregion
+
+        #region Properties
+
         [Inject]
         public IAuthProvider AuthProvider { get; set; }
+
+        #endregion
     }
 }
