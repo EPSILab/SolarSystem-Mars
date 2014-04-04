6a8226f0afe29e8e3f19a05b4042439d956608ab
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-04-04 15:51:36 +0200

    Create controller for links

--- Mars.ViewController/Controllers/LinksController.cs
+++ Mars.ViewController/Controllers/LinksController.cs
@@ -1,7 +1,13 @@
 ﻿using SolarSystem.Mars.Model.ManagersService;
 using SolarSystem.Mars.Model.Model.Abstract;
+using SolarSystem.Mars.ViewController.Exceptions;
 using SolarSystem.Mars.ViewController.Infrastructure.Concrete;
+using SolarSystem.Mars.ViewController.Resources;
+using SolarSystem.Mars.ViewController.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace SolarSystem.Mars.ViewController.Controllers
@@ -11,97 +17,181 @@ namespace SolarSystem.Mars.ViewController.Controllers
     {
         #region Constructor
 
-        public LinksController(IReaderLimit<Link> lienManager)
+        /// <summary>
+        /// Constructor. Parameters are resolved with NInject
+        /// </summary>
+        public LinksController(IReaderLimit<Link> model, IConstants constants)
+            : base(constants)
         {
-            _lienManager = lienManager;
+            _model = model;
         }
 
         #endregion
 
         #region Attributes
 
-        private readonly IReaderLimit<Link> _lienManager;
+        /// <summary>
+        /// Main model
+        /// </summary>
+        private readonly IReaderLimit<Link> _model;
 
         #endregion
 
-        #region Methods
+        #region Index methods
 
-        // GET: /Links/
-        public ActionResult Index()
+        /// <summary>
+        /// GET: /Link/
+        /// GET: /Link/Index
+        /// GET: /Link/Index/10
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Index(int id = 0)
         {
-            IEnumerable<Link> liens = _lienManager.Get(1, 10);
-            return View(liens);
+            // Get Link and tranform them in LinkViewModel
+            IEnumerable<Link> listLink = _model.Get(id, _constants.ItemsNumber);
+            IEnumerable<LinkViewModel> vm = listLink.Select(link => new LinkViewModel(link));
+
+            // Send Id and ItemsNumber for navigation
+            ViewBag.Id = id;
+            ViewBag.ItemsNumber = _constants.ItemsNumber;
+
+            return View(vm);
         }
 
-        // GET: /Links/Manage
-        public ActionResult Manage()
+        #endregion
+
+        #region Manage methods
+
+        /// <summary>
+        /// GET: /Link/Manage : Create a new link
+        /// GET: /Link/Manage/5 : Edit an existing link
+        /// </summary>
+        /// <param name="id">Link's Id to manage. If Id = 0, it is a new link</param>
+        public ActionResult Manage(int id = 0)
         {
-            return View();
+            LinkViewModel vm;
+
+            // Create a link
+            if (id == 0)
+                vm = new LinkViewModel();
+            // Edit an existing link
+            else
+            {
+                Link link = _model.Get(id);
+                vm = new LinkViewModel(link);
+            }
+
+            return View(vm);
         }
 
-        // POST: /Link/Manage
+        /// <summary>
+        /// POST: /Link/Manage
+        /// Raised when the user has clicked on the OK button
+        /// </summary>
+        /// <param name="vm">Page ViewModel</param>
+        /// <param name="file">Image to send on the server</param>
         [HttpPost]
-        public ActionResult Manage(Link lien)
+        public ActionResult Manage(LinkViewModel vm, HttpPostedFileBase file)
         {
             try
             {
-                // TODO: Add insert logic here
-                _lienManager.Add(lien, AuthProvider.LoginViewModel.Username, AuthProvider.LoginViewModel.PasswordCrypted);
+                if (!ModelState.IsValid)
+                    throw new InvalidModelStateException();
+
+                // Prepare to send the link to the server
+                Link link = new Link
+                {
+                    Id = vm.Id,
+                    Description = vm.Description,
+                    ImageUrl = vm.ImageRemoteUrl,
+                    Label = vm.Label,
+                    Order = vm.Order,
+                    Url = vm.Url
+                };
+
+                // Image management
+                SendImageToServer(vm, file, link);
+
+                // Save the link
+                LoginViewModel loginVM = AuthProvider.LoginViewModel;
+
+                if (vm.Id == 0)
+                {
+                    _model.Add(link, loginVM.Username, loginVM.PasswordCrypted);
+                    ViewBag.SuccessMessage = MessagesResources.LinkCreated;
+                }
+                else
+                {
+                    _model.Edit(link, loginVM.Username, loginVM.PasswordCrypted);
+                    ViewBag.SuccessMessage = MessagesResources.LinkUpdated;
+                }
 
                 return RedirectToAction("Index");
             }
-            catch
+            catch (InvalidModelStateException ex)
             {
-                return View(lien);
+                ViewBag.ErrorMessage = ex.DisplayMessage;
             }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+            }
+
+            return View(vm);
         }
 
-        // GET: /Links/Edit/5
-        public ActionResult Manage(int id)
-        {
-            Link lien = _lienManager.Get(id);
-            return View(lien);
-        }
+        #endregion
 
-        // POST: /Link/Edit/5
+        #region Delete methods
+
+        /// <summary>
+        ///  GET: /Link/Delete/1
+        /// Delete an existing link
+        /// </summary>
+        /// <param name="id">Link Id to delete</param>
         [HttpPost]
-        public ActionResult Manage(int id, Link lien)
+        public JsonResult Delete(int id)
         {
             try
             {
-                // TODO: Add update logic here
-                _lienManager.Edit(lien, AuthProvider.LoginViewModel.Username, AuthProvider.LoginViewModel.PasswordCrypted);
+                LoginViewModel loginVM = AuthProvider.LoginViewModel;
+                // TODO: Uncomment line bellow
+                //_model.Delete(id, loginVM.Username, loginVM.PasswordCrypted);
 
-                return RedirectToAction("Index");
+                return Json(new { id, success = true, message = MessagesResources.LinkDeleted });
             }
-            catch
+            catch (Exception ex)
             {
-                return View(lien);
+                return Json(new { id = 0, success = false, message = ex.Message });
             }
         }
 
-        // GET: /Links/Delete/5
-        public ActionResult Delete(int id)
-        {
-            Link lien = _lienManager.Get(id);
-            return RedirectToAction("Index");
-        }
+        #endregion
 
-        // POST: /Link/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, Link lien)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-                _lienManager.Delete(id, AuthProvider.LoginViewModel.Username, AuthProvider.LoginViewModel.PasswordCrypted);
+        #region General Methods
 
-                return RedirectToAction("Index");
-            }
-            catch
+        /// <summary>
+        /// Send Image to the server
+        /// </summary>
+        /// <param name="vm">LinkViewModel corresponding to the link</param>
+        /// <param name="file">File - Image could be sent</param>
+        /// <param name="link">Link that will get image</param>
+        private void SendImageToServer(LinkViewModel vm, HttpPostedFileBase file, Link link)
+        {
+            // Image is local
+            if (file != null && file.ContentLength > 0)
             {
-                return RedirectToAction("Index");
+                string imagePath = string.Format("../Images/Link/{0}", file.FileName);
+                file.SaveAs(imagePath);
+                link.ImageUrl = imagePath;
             }
+            // Image is remote
+            else if (!string.IsNullOrWhiteSpace(vm.ImageRemoteUrl))
+                link.ImageUrl = vm.ImageRemoteUrl;
+            // No image given
+            else
+                throw new InvalidModelStateException(ErrorRessources.ImageRequired);
         }
 
         #endregion
