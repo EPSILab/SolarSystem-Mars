9be1107fe412625736d91159bfdf5b97894bb18e
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-04-06 15:22:32 +0200

    Create password view-model

---
+++ Mars.ViewController/ViewModels/PasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SolarSystem.Mars.ViewController.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace SolarSystem.Mars.ViewController.ViewModels
+{
+    public class PasswordViewModel
+    {
+        /// <summary>
+        /// Password
+        /// </summary>
+        [Required(ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "PasswordRequired")]
+        [MinLength(6, ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "PasswordMinLength")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Password confirmation
+        /// </summary>
+        [Required(ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "ConfirmPasswordRequired")]
+        [Compare("Password", ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "SamePasswords")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
