067405d48e17e3b8d3e2f06106c9f57fee261b4f
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-04-06 15:09:34 +0200

    Create reset password view and view-model

---
+++ Mars.ViewController/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SolarSystem.Mars.ViewController.Resources;
+
+namespace SolarSystem.Mars.ViewController.ViewModels
+{
+    /// <summary>
+    /// View-model for password reset
+    /// </summary>
+    public class ResetPasswordViewModel
+    {
+        /// <summary>
+        /// Username
+        /// </summary>
+        [Display(ResourceType = typeof(ContentRessources), Name = "Username")]
+        [Required(ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "UsernameRequired")]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        [Required(ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "PasswordRequired")]
+        [MinLength(6, ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "PasswordMinLength")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Password confirmation
+        /// </summary>
+        [Required(ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "ConfirmPasswordRequired")]
+        [Compare("Password", ErrorMessageResourceType = typeof(ErrorRessources), ErrorMessageResourceName = "SamePasswords")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Key
+        /// </summary>
+        public string Key { get; set; }
+    }
+}
